d18e3f50079a435944db47627b371196fa872909
Julia Gomez <123409109+julgom@users.noreply.github.com>
2024-07-27 16:55:21 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 
+
 import google.generativeai as genai
 import os
 import PyPDF2 as pdf
@@ -27,7 +28,6 @@ def input_pdf_text(uploaded_file):
 
 
 
-
 ## streamlit app
 
 st.set_page_config(page_title="Resume Coach")
@@ -35,8 +35,6 @@ st.header("Resume Coach")
 st.write("Upload your resume and enter your email for a review.")
 
 email = st.text_input("Email")
-
-input_text=st.text_area("Job Description: ",key="input")
 uploaded_file=st.file_uploader("Choose a file(PDF)",type=["pdf"])
 
 
@@ -44,40 +42,32 @@ if uploaded_file is not None:
     st.write("PDF Uploaded Successfully")
 
 
-submit1 = st.button("Tell Me About the Resume")
+submit = st.button("Submit")
 
-#submit2 = st.button("How Can I Improvise my Skills")
-
-submit3 = st.button("Percentage match")
-
-input_prompt1 = """
- You are an experienced Technical Human Resource Manager,your task is to review the provided resume against the job description. 
-  Please share your professional evaluation on whether the candidate's profile aligns with the role. 
- Highlight the strengths and weaknesses of the applicant in relation to the specified job requirements.
+input_prompt = """You are an AI Resume Coach, an expert in helping individuals 
+create compelling and professional resumes. Your primary role is to guide users in enhancing 
+their resumes to improve their chances of securing job interviews. Here are your key tasks:
+Resume Review: Carefully analyze the user's existing resume, providing constructive feedback on formatting, 
+content, and overall presentation. Content Enhancement: Offer specific suggestions to improve the language, 
+tone, and clarity of each section. Focus on making job responsibilities and achievements more impactful using 
+action verbs and quantifiable results. Industry-Specific Advice: Tailor your recommendations based on the user’s 
+target industry and role, ensuring that the resume meets industry standards and highlights relevant skills and 
+experiences. Keyword Optimization: Help the user optimize their resume with appropriate keywords to pass through 
+Applicant Tracking Systems (ATS). Section Organization: Guide the user on the most effective way to organize their 
+resume sections, such as Professional Summary, Skills, Work Experience, Education, and Additional Information. 
+Question Answering: Respond to specific queries from users regarding resume writing best practices, such as how to 
+handle employment gaps, career changes, or lack of experience. Here is a resume to reveiw below:
 """
 
-input_prompt3 = """
-You are an skilled ATS (Applicant Tracking System) scanner with a deep understanding of data science and ATS functionality, 
-your task is to evaluate the resume against the provided job description. give me the percentage of match if the resume matches
-the job description. First the output should come as percentage and then keywords missing and last final thoughts.
-"""
-
-if submit1:
-    if uploaded_file is not None and email is not None and input_text is not None:
+if submit:
+    if uploaded_file and email:
         pdf_content=input_pdf_text(uploaded_file)
-        response=get_gemini_response(input_prompt1+"Resume"+pdf_content+"Job Description"+input_text)
+        response=get_gemini_response(input_prompt + " " + pdf_content)
         st.subheader("The Response is")
         st.write(response)
+    elif uploaded_file and not email:
+       st.write("Please enter the email")
+    elif not uploaded_file and email:
+       st.write("Please upload the resume")
     else:
        st.write("Please enter all details")
-
-elif submit3:
-    if uploaded_file is not None and email is not None and input_text is not None:
-        pdf_content=input_pdf_text(uploaded_file)
-        response=get_gemini_response(input_prompt3+"Resume"+pdf_content+"Job Description"+input_text)
-        st.subheader("The Response is")
-        st.write(response)
-    else:
-       st.write("Please enter all details")
-
-
