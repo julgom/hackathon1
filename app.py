6e3ec931c7b20dbe6c9b5c23aa0dc136ca3c56c2
Julia Gomez <123409109+julgom@users.noreply.github.com>
2024-07-27 21:52:25 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,48 +1,16 @@
 import streamlit as st
-from bs4 import BeautifulSoup
-import shutil
-import pathlib
-import logging
-
 import google.generativeai as genai
 import os
 import PyPDF2 as pdf
 import fitz  # PyMuPDF
 import json
 from dotenv import load_dotenv
+from utils import add_analytics_tag
 
-def add_analytics_tag():
-    # replace G-XXXXXXXXXX to your web app's ID
-    
-    analytics_js = """
-   
-    <!-- Google tag (gtag.js) -->
-    <script async src="https://www.googletagmanager.com/gtag/js?id=G-XSZKX1MP02"></script>
-    <script>
-      window.dataLayer = window.dataLayer || [];
-      function gtag(){dataLayer.push(arguments);}
-      gtag('js', new Date());
+# Call the function to add Google Analytics tag
+add_analytics_tag()
 
-      gtag('config', 'G-XSZKX1MP02');
-    </script>
-    
-    """
-    analytics_id = "G-XSZKX1MP02"
-    
-    # Identify html path of streamlit
-    index_path = pathlib.Path(st.__file__).parent / "static" / "index.html"
-    logging.info(f'editing {index_path}')
-    soup = BeautifulSoup(index_path.read_text(), features="html.parser")
-    if not soup.find(id=analytics_id): # if id not found within html file
-        bck_index = index_path.with_suffix('.bck')
-        if bck_index.exists():
-            shutil.copy(bck_index, index_path)  # backup recovery
-        else:
-            shutil.copy(index_path, bck_index)  # save backup
-        html = str(soup)
-        new_html = html.replace('<head>', '<head>\n' + analytics_js) 
-        index_path.write_text(new_html) # insert analytics tag at top of head
-        
+
 load_dotenv()
 
 genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
